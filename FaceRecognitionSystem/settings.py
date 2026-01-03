9f9f8ea345692a02aa8b9c2a4a7304efd1ab698b
kunjladani05 <ladanikunj6019@gmail.com>
2026-01-03 11:59:17 +0530

    change in setting.py file

--- FaceRecognitionSystem/settings.py
+++ FaceRecognitionSystem/settings.py
@@ -12,16 +12,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-89ob*1x_-6+ntf_67nqske!00vvhoi02uimi5_x@^en@ij-gjq'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
